dd0efbbb34a1fb3339eddb8465cce7a8d1a10df3
Nishanth Samala <nishanth.samala@gmail.com>
2020-06-26 01:17:11 -0400

    quick fix to load controls

--- src/controls.js
+++ src/controls.js
@@ -1,5 +1,4 @@
 const player = document.getElementById('hlsjsContent')
-
 let playing = true
 const seekSmall = 5							// 5 second jump on left / right arrows
 const seekLarge = 10						// 10 second jump on 'j'/ 'l' keys
@@ -13,35 +12,33 @@ Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
 	}
 })
 
-const setupControls = () => {
-	// keyboard shortcuts
-	window.addEventListener('keydown', event => {
-		if (event.key === ' ')
-			player.playing ? player.pause() : player.play()
-		else if (event.key === 'm')
-			player.muted = !player.muted
-		else if (event.key === 'j')
-			player.currentTime -= seekLarge
-		else if (event.key === 'l')
-			player.currentTime += seekLarge
-		else if (event.key === 'ArrowRight')
-			player.currentTime += seekSmall
-		else if (event.key === 'ArrowLeft')
-			player.currentTime -= seekSmall
-		else if (event.key === '+' || event.key === '=')
-			player.volume = Math.min(1.0, player.volume + volumeChange)
-		else if (event.key === '-' || event.key === '_')
-			player.volume = Math.max(0.0, player.volume - volumeChange)
-		else if (event.key === 'f') {
-			if (player.requestFullscreen) {
-				player.requestFullscreen();
-			} else if (player.mozRequestFullScreen) {
-				player.mozRequestFullScreen();
-			} else if (player.webkitRequestFullscreen) {
-				player.webkitRequestFullscreen();
-			} else if (player.msRequestFullscreen) { 
-				player.msRequestFullscreen();
-			}
-		}
-	})
-}
+
+window.addEventListener('keydown', event => {
+  if (event.key === ' ')
+    player.playing ? player.pause() : player.play()
+  else if (event.key === 'm')
+    player.muted = !player.muted
+  else if (event.key === 'j')
+    player.currentTime -= seekLarge
+  else if (event.key === 'l')
+    player.currentTime += seekLarge
+  else if (event.key === 'ArrowRight')
+    player.currentTime += seekSmall
+  else if (event.key === 'ArrowLeft')
+    player.currentTime -= seekSmall
+  else if (event.key === '+' || event.key === '=')
+    player.volume = Math.min(1.0, player.volume + volumeChange)
+  else if (event.key === '-' || event.key === '_')
+    player.volume = Math.max(0.0, player.volume - volumeChange)
+  else if (event.key === 'f') {
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.mozRequestFullScreen) {
+      player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) {
+      player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) { 
+      player.msRequestFullscreen();
+    }
+  }
+})
